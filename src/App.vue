2c5c2dbcfcae65a0cc31e07388807e9dda923b80
elskyu <aditiyapratama2014@gmail.com>
2024-07-11 09:17:32 +0700

    commmmm

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,21 @@
-<script>
-import { computed } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-
-export default {
-  setup() {
-    const route = useRoute();
-    const router = useRouter();
-
-    const showNavbar = computed(() => {
-      return route.name !== 'login';
-    });
-
-    const logout = () => {
-      localStorage.removeItem('auth'); // Contoh untuk menghapus autentikasi
-      router.push({ name: 'login' });
-    };
-
-    return {
-      showNavbar,
-      logout,
-    };
-  }
-}
-</script>
-
+<!-- App.vue -->
 <template>
-  <div>
-    <nav v-if="showNavbar" class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-      <div class="container">
-        <router-link :to="{ name: 'home' }" class="navbar-brand">HOME</router-link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link :to="{ name: 'posts.index' }" class="nav-link active" aria-current="page">POSTS</router-link>
-            </li>
-          </ul>
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0" role="search">
-            <a
-              href="https://santrikoding.com"
-              target="_blank"
-              class="btn btn-success"
-              >SANTRIKODING.COM</a>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <!--- render router view -->
-    <router-view></router-view>
-
+  <div id="app">
+    <Sidebar />
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script>
+import Sidebar from './components/Sidebar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+  },
+};
+</script>
+
+<style>
+/* Global styles can go here */
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,7 @@
       <nav class="sidebar custom-sidebar">
         <ul class="navbar-nav">
           <li class="nav-item">
-          <router-link :to="{ name: 'pegawai.dasboard' }" class="nav-link active" style="font-weight: bold; margin-left: 25px;">
+          <router-link :to="{ name: '' }" class="nav-link active" style="font-weight: bold; margin-left: 25px;">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon">
               <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
               </svg>
@@ -11,7 +11,7 @@
           </router-link>
           </li>
           <li class="nav-item">
-            <router-link :to="{ name: 'posts.index' }" class="nav-link active" aria-current="page" style="font-weight: bold;  margin-left: 25px;">
+            <router-link :to="{ name: '' }" class="nav-link active" aria-current="page" style="font-weight: bold;  margin-left: 25px;">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon">
               <path fill-rule="evenodd" d="M5.25 2.25a3 3 0 0 0-3 3v4.318a3 3 0 0 0 .879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 0 0 5.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 0 0-2.122-.879H5.25ZM6.375 7.5a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" clip-rule="evenodd" />
               </svg>  
@@ -19,7 +19,7 @@
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link :to="{ name: 'about' }" class="nav-link active" aria-current="page" style="font-weight: bold;  margin-left: 25px;">
+            <router-link :to="{ name: '' }" class="nav-link active" aria-current="page" style="font-weight: bold;  margin-left: 25px;">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
               </svg>
@@ -27,7 +27,7 @@
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link :to="{ name: 'about' }" class="nav-link active" aria-current="page" style="font-weight: bold;  margin-left: 25px;">
+            <router-link :to="{ name: '' }" class="nav-link active" aria-current="page" style="font-weight: bold;  margin-left: 25px;">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
               </svg>
@@ -35,7 +35,7 @@
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link :to="{ name: 'about' }" class="nav-link active" aria-current="page" style="font-weight: bold;  margin-left: 25px;">
+            <router-link :to="{ name: '' }" class="nav-link active" aria-current="page" style="font-weight: bold;  margin-left: 25px;">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
               </svg>
@@ -43,7 +43,7 @@
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link :to="{ name: 'about' }" class="nav-link active" aria-current="page" style="font-weight: bold;  margin-left: 25px;">
+            <router-link :to="{ name: '' }" class="nav-link active" aria-current="page" style="font-weight: bold;  margin-left: 25px;">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
               </svg>
