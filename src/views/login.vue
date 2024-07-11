0ad845b428c8ae661eb67642a84fe7dceb0c46f6
elskyu <aditiyapratama2014@gmail.com>
2024-07-11 10:30:33 +0700

    login

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,6 +2,10 @@
 import { ref } from 'vue';
 import axios from 'axios';
 import '../style/login.css'; // Import specific CSS file for the login component
+import '../style/font.css';
+
+
+import imageSrc from '../images/login_icon.svg';
 
 const name = ref('');
 const email = ref('');
@@ -37,23 +41,29 @@ const handleRegister = async () => {
 <template>
     <div class="login-container mt-5">
         <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card border-0 rounded shadow">
+            <div class="col-md-6">              
                     <div class="card-body">
-                        <h4 class="login-card-title text-center mb-4">LOGIN</h4>
-                        <form @submit.prevent="handleLogin">
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="email" v-model="email" autocomplete="email" required>
+                        <div class="login-card-title text-center mb-4">
+                            <div class="image">
+                                <img :src="imageSrc" alt="Profile Image" />
                             </div>
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" v-model="password" autocomplete="current-password" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary w-100">LOGIN</button>
+                        </div>
+                        <form @submit.prevent="login">
+                        <div class="mb-3 input-container">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="input-icon">
+                                <path fill-rule="evenodd" d="M5.25 2.25a3 3 0 0 0-3 3v4.318a3 3 0 0 0 .879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 0 0 5.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 0 0-2.122-.879H5.25ZM6.375 7.5a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" clip-rule="evenodd"/>
+                            </svg>
+                            <input type="email" placeholder="username" class="form-control" id="email" v-model="email" autocomplete="email" required>
+                        </div>
+                        <div class="mb-3 input-container">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="input-icon">
+                                <path fill-rule="evenodd" d="M5.25 2.25a3 3 0 0 0-3 3v4.318a3 3 0 0 0 .879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 0 0 5.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 0 0-2.122-.879H5.25ZM6.375 7.5a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" clip-rule="evenodd"/>
+                            </svg>
+                            <input type="password" placeholder="password" class="form-control" id="password" v-model="password" autocomplete="current-password" required>
+                        </div>
+                            <button type="submit" class="btn btn-primary">LOGIN</button>
                         </form>
                     </div>
-                </div>
             </div>
         </div>
     </div>
