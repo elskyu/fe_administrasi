0f84558d7f97948579a5768511bf533ddb9704dc
Tempebenguk <erricksb592@gmail.com>
2024-07-11 16:35:56 +0700

    configurasi praimvu

--- src/main.js
+++ src/main.js
@@ -7,10 +7,16 @@ import App from './App.vue';
 //import config router
 import router from './router'
 
+//ngimpor praim viyu
+import PrimeVue from 'primevue/config';
+
 //create App Vue
 const app = createApp(App);
 
 //gunakan "router" di Vue dengan plugin "use"
 app.use(router);
+
+
+app.use(PrimeVue);
 
 app.mount('#app');
