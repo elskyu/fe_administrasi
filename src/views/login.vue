2582de0077e9f893c0a57858dac0d01b1d24c00a
Rofaalvin <rofaalvin22@gmail.com>
2024-07-12 08:48:14 +0700

    pp

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,22 +1,33 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
-import '../style/login.css';
+import '../style/login.css'; // Import specific CSS file for the login component
 import '../style/font.css';
-import InputText from 'primevue/inputtext';
+
+
 import imageSrc from '../images/login_icon.svg';
 
+const name = ref('');
 const email = ref('');
 const password = ref('');
-const handleLogin = async () => {
+const confirmPassword = ref('');
+
+const handleRegister = async () => {
+//   if (password.value !== confirmPassword.value) {
+//     alert('Passwords do not match');
+//     return;
+//   }
+
   try {
-    const response = await axios.post('http://localhost:8000/api/login', {
+    const response = await axios.post('http://localhost:8000/api/register', {
+      name: name.value,
       email: email.value,
       password: password.value
     });
 
     if (response.data.success) {
-      alert('Login successful');
+      alert('Registration successful');
+      // Handle redirection to login or any other page upon successful registration
     } else {
       alert(response.data.message || 'Registration failed');
     }
@@ -51,6 +62,7 @@ const handleLogin = async () => {
                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="input-icon">
                             <path fill-rule="evenodd" d="M15.75 1.5a6.75 6.75 0 0 0-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 0 0-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 0 0 .75-.75v-1.5h1.5A.75.75 0 0 0 9 19.5V18h1.5a.75.75 0 0 0 .53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1 0 15.75 1.5Zm0 3a.75.75 0 0 0 0 1.5A2.25 2.25 0 0 1 18 8.25a.75.75 0 0 0 1.5 0 3.75 3.75 0 0 0-3.75-3.75Z" clip-rule="evenodd" />
                           </svg>
+
                             <input type="password" placeholder="password" class="form-control" id="password" v-model="password" autocomplete="current-password" required>
                         </div>
                             <button type="submit" class="btn btn-primary">LOGIN</button>
